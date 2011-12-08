b02abed0f72ff22ce21efa30981417193cc4b749
Salim Hermidas <s.hermidas@gmail.com>
2011-12-08 20:47:05 +0100

    ... some testing stuff

--- src/serie5/C.java
+++ src/serie5/C.java
@@ -2,6 +2,6 @@ package serie5;
 
 class C extends B{
 public C(){ super (); }
-public void foo (){ System .out . println ("C.foo "); }
-//public void foo (){ this .bar (); }
+//public void foo (){ System .out . println ("C.foo "); }
+public void foo (){ this .bar (); }
 }
