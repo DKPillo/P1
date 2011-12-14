e4d0632f71248c28501f579685bfd8c10a37b944
Salim Hermidas <s.hermidas@gmail.com>
2011-12-14 15:59:40 +0100

    FactBig works up to an input of 8964

--- src/serie6/FactBig.java
+++ src/serie6/FactBig.java
@@ -14,6 +14,7 @@ import java.math.BigInteger;
 public class FactBig {
 
 	public static void main(String[] args) {
+		//WORKS TO java FactBig 8964
 		BigInteger a;
 		BigInteger b = new BigInteger(String.valueOf(1));
 		try {
